2ca628fa68bcc0c6a0a9d2bbc779d68b1cda33de
indah mariana nainggolan <73211222+indahmariana@users.noreply.github.com>
2020-11-23 23:04:17 +0700

    Update club.sql

--- club.sql
+++ club.sql
@@ -12,6 +12,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -34,12 +35,36 @@ CREATE TABLE `akun` (
   `Email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+--
+-- Struktur dari tabel `profil`
+--
+
+CREATE TABLE `profil` (
+  `nis` int(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `agama` varchar(20) NOT NULL,
+  `jenis_club` varchar(20) NOT NULL,
+  `jabatan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Dumping data untuk tabel `akun`
 --
 
 INSERT INTO `akun` (`NISN`, `Password`, `Nama`, `Email`) VALUES
 (1234567890, 'imk123', 'Ineztri', 'inez@gmail.com');
+
+--
+-- Dumping data for table `profil`
+--
+
+INSERT INTO `profil` (`nis`, `email`, `kelas`, `jenis_kelamin`, `alamat`, `agama`, `jenis_club`, `jabatan`) VALUES
+(19140203, 'inez@gmail.com', 'x mipa 1', 'perempuan', 'jalan jamin ginting', 'kristen', 'matematika', 'ketua');
+
 
 --
 -- Indexes for dumped tables
@@ -50,6 +75,17 @@ INSERT INTO `akun` (`NISN`, `Password`, `Nama`, `Email`) VALUES
 --
 ALTER TABLE `akun`
   ADD PRIMARY KEY (`NISN`);
+--
+-- Indeks untuk tabel `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`nis`);
+--
+-- AUTO_INCREMENT for table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `nis` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19140204;
+COMMIT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
